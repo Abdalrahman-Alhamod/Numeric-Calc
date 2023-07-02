72f0c276c62606126a4fbe547c72a698fe3ea799
Abd-HM <abd.alrrahman.alhamod@gmail.com>
2023-07-02 13:47:15 +0300

    Add simpson 1/3 integral implementation method

--- src/Integral.java
+++ src/Integral.java
@@ -23,4 +23,20 @@ public class Integral {
 
         return sum * (h / 2);
     }
+
+    public static double getSimpson3(Function func, double a, double b, double n) {
+        double h = (b - a) / n;
+        double sum = 0;
+        sum += func.getValueAt(a) + func.getValueAt(b);
+        double value = a + h;
+        for (int i = 1; i <= n - 1; i++) {
+            if (i % 2 == 0)
+                sum += 2 * func.getValueAt(value);
+            else
+                sum += 4 * func.getValueAt(value);
+            value += h;
+        }
+
+        return sum * (h / 3);
+    }
 }
