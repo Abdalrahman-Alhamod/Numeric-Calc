069fce0cff085a58137a7163194a2e0dd0cc8247
Abd-HM <abd.alrrahman.alhamod@gmail.com>
2023-07-02 13:46:42 +0300

    Add trapezoidal integral implementation method

--- src/Integral.java
+++ src/Integral.java
@@ -10,4 +10,17 @@ public class Integral {
 
         return sum * h;
     }
+
+    public static double getTraps(Function func, double a, double b, double n) {
+        double h = (b - a) / n;
+        double sum = 0;
+        sum += func.getValueAt(a) + func.getValueAt(b);
+        double value = a + h;
+        for (int i = 1; i <= n - 1; i++) {
+            sum += 2 * func.getValueAt(value);
+            value += h;
+        }
+
+        return sum * (h / 2);
+    }
 }
