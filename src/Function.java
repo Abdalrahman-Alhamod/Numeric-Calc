ce24293ea0b2c2f42aead9b3f3d968ff5919565c
Abd-HM <abd.alrrahman.alhamod@gmail.com>
2023-07-02 14:07:15 +0300

    Add x points and y points Arraylists to Function class

--- src/Function.java
+++ src/Function.java
@@ -1,12 +1,23 @@
+import java.util.ArrayList;
+
 public class Function {
+
+    private ArrayList<Double> xp;
+    private ArrayList<Double> yp;
+
     public Function(String f) {
 
+    }
+
+    public Function(ArrayList<Double> xp, ArrayList<Double> yp) {
+        this.xp = xp;
+        this.yp = yp;
     }
 
     public double getValueAt(double x) {
         //System.out.println("Value : "+x);
         //System.out.println("Func : "+Math.log(x));
-        return (1 / x);
+        return yp.get(xp.indexOf(x));
     }
 
     public double getDiffAt(double x, int rank) {
