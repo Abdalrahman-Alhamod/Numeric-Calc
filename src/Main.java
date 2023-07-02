a2544bfc98b770d551c40e6a8279d342bfb0f92a
Abd-HM <abd.alrrahman.alhamod@gmail.com>
2023-07-02 18:48:10 +0300

    Testing Polynomial methods

--- src/Main.java
+++ src/Main.java
@@ -5,7 +5,7 @@ import static java.lang.Double.*;
 
 public class Main {
     public static void main(String[] args) {
-        Scanner in = new Scanner(System.in);
+        /*Scanner in = new Scanner(System.in);
         double a, b, n, temp;
         ArrayList<Double> xp = new ArrayList<>(), yp = new ArrayList<>();
         System.out.print("Enter number of points : ");
@@ -30,7 +30,15 @@ public class Main {
         System.out.println("Traps : " + Integral.getTraps(func, a, b, n));
         System.out.println("Simpson 1/3 : " + Integral.getSimpson3(func, a, b, n));
         System.out.println("Simpson 3/8 : " + Integral.getSimpson8(func, a, b, n));
-        System.out.println("Paul : " + Integral.getPaul(func, a, b, n));
+        System.out.println("Paul : " + Integral.getPaul(func, a, b, n));*/
         // 0 0 0.1 0.0001 0.2 0.0016 0.3 0.0081 0.4 0.0256 0.5 0.0625 0.6 0.1296
+
+        ArrayList<Double> coeffs = new ArrayList<>();
+        coeffs.add(1.0);
+        coeffs.add(2.0);
+        coeffs.add(3.0);
+        Polynomial p = new Polynomial(coeffs);
+        System.out.println(p.evaluate(2));
+        System.out.println(p);
     }
 }
