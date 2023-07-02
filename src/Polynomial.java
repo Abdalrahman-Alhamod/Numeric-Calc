093a5002d92275e7c3a1a2f644e40f5dd5d5bee9
Abd-HM <abd.alrrahman.alhamod@gmail.com>
2023-07-02 18:37:43 +0300

    Add comment to evaluate in Polynomial

--- src/Polynomial.java
+++ src/Polynomial.java
@@ -11,6 +11,10 @@ public class Polynomial {
     public static double evaluate(double x) {
         double res = 0.0;
         for (int i = 0; i < coeffs.size(); i++) {
+            // Compute the contribution of the current term to the result and add it to the running sum
+            // The contribution is the coefficient times x raised to the power of the degree of the current term
+            // The degree of the current term is the number of terms remaining after the current term
+            // minus one (since the degree of the highest-order term is zero)
             res += coeffs.get(i) * Math.pow(x, coeffs.size() - i - 1);
         }
         return res;
