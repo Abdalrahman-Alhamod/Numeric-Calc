5652a2fae009a47b90ef5e17a02054d970813feb
Abd-HM <abd.alrrahman.alhamod@gmail.com>
2023-07-02 13:45:38 +0300

    Add Rectangle integral implementation method

--- src/Integral.java
+++ src/Integral.java
@@ -1,3 +1,13 @@
 public class Integral {
+    public static double getRect(Function func, double a, double b, double n) {
+        double h = (b - a) / n;
+        double sum = 0;
+        double value = a;
+        for (int i = 0; i <= n - 1; i++) {
+            sum += func.getValueAt(value);
+            value += h;
+        }
 
+        return sum * h;
+    }
 }
