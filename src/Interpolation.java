2e3e3f7ce416f13760a277521a0e5790a0ac093b
Abd-HM <abd.alrrahman.alhamod@gmail.com>
2023-07-03 00:20:55 +0300

    Add getGeneralMethod to Interpolation class

--- src/Interpolation.java
+++ src/Interpolation.java
@@ -1,3 +1,16 @@
+import java.util.ArrayList;
+
 public class Interpolation {
-    
+    static Polynomial getGeneralMethod(Function func) {
+        ArrayList<Double> xp = func.getXp();
+        ArrayList<Double> yp = func.getYp();
+        Matrix SE = new Matrix(Matrix.getVandermonde(xp).getData());
+        for (int i = 0; i < yp.size(); i++) {
+            SE.getRow(i).add(yp.get(i));
+        }
+        SE.setColsNum(SE.getColsNum() + 1);
+        ArrayList<Double> coeffs = SE.solve();
+        System.out.println("Interpolated Function using the general method : ");
+        return new Polynomial(coeffs);
+    }
 }
