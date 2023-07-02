da672edf36569a48cb8e5925d5b8ef98d8b0fc38
Abd-HM <abd.alrrahman.alhamod@gmail.com>
2023-07-02 18:40:47 +0300

    Add override toString method to Polynomial with comments

--- src/Polynomial.java
+++ src/Polynomial.java
@@ -19,4 +19,40 @@ public class Polynomial {
         }
         return res;
     }
+
+    @Override
+    public String toString() {
+        StringBuilder sb = new StringBuilder(); // Create a new StringBuilder to build the string representation
+        int n = coeffs.size() - 1; // Get the degree of the polynomial (the size of the coefficient ArrayList minus one)
+        for (int i = 0; i <= n; i++) { // Loop over each coefficient from the constant term to the highest-order term
+            double coeff = coeffs.get(i); // Get the current coefficient
+            if (coeff != 0) { // If the coefficient is nonzero
+                if (i == 0) { // If the term is the constant term
+                    sb.append(coeff); // Append the coefficient
+                } else if (i == 1) { // If the term is the linear term
+                    if (coeff == 1) { // If the coefficient is 1
+                        sb.append("x"); // Append the variable symbol
+                    } else if (coeff == -1) { // If the coefficient is -1
+                        sb.append("-x"); // Append the negative variable symbol
+                    } else { // If the coefficient is not 1 or -1
+                        sb.append(coeff).append("x"); // Append the coefficient and variable symbol
+                    }
+                } else { // If the term is a higher-order term
+                    if (coeff == 1) { // If the coefficient is 1
+                        sb.append("x^").append(i); // Append the variable symbol and exponent
+                    } else if (coeff == -1) { // If the coefficient is -1
+                        sb.append("-x^").append(i); // Append the negative variable symbol and exponent
+                    } else { // If the coefficient is not 1 or -1
+                        sb.append(coeff).append("x^").append(i); // Append the coefficient, variable symbol, and exponent
+                    }
+                }
+                if (i < n && coeffs.get(i + 1) > 0) { // If there are more terms and the next coefficient is positive
+                    sb.append(" + "); // Append the addition operator with spacing
+                } else if (i < n && coeffs.get(i + 1) < 0) { // If there are more terms and the next coefficient is negative
+                    sb.append(" - "); // Append the subtraction operator with spacing
+                }
+            }
+        }
+        return sb.toString(); // Return the final string representation
+    }
 }
