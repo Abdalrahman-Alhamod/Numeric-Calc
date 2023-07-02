b8b2f5de3886c0044bad084111223b94e7df1492
Abd-HM <abd.alrrahman.alhamod@gmail.com>
2023-07-02 18:43:50 +0300

    Removing static

--- src/Polynomial.java
+++ src/Polynomial.java
@@ -2,13 +2,13 @@ import java.util.ArrayList;
 import java.util.Objects;
 
 public class Polynomial {
-    private static ArrayList<Double> coeffs = null;
+    private ArrayList<Double> coeffs = null;
 
     public Polynomial(ArrayList<Double> coeffs) {
-        Polynomial.coeffs = Objects.requireNonNull(coeffs, "coeffs cannot be null");
+        this.coeffs = Objects.requireNonNull(coeffs, "coeffs cannot be null");
     }
 
-    public static double evaluate(double x) {
+    public double evaluate(double x) {
         double res = 0.0;
         for (int i = 0; i < coeffs.size(); i++) {
             // Compute the contribution of the current term to the result and add it to the running sum
