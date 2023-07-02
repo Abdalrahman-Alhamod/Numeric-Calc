b6cbd876544eae85736d6f9ca52fc0a720bb45df
Abd-HM <abd.alrrahman.alhamod@gmail.com>
2023-07-02 13:48:32 +0300

    Add paul integral implementation method

--- src/Integral.java
+++ src/Integral.java
@@ -55,4 +55,22 @@ public class Integral {
 
         return sum * 3 * (h / 8);
     }
+
+    public static double getPaul(Function func, double a, double b, double n) {
+        double h = (b - a) / n;
+        double sum = 0;
+        sum += 7 * func.getValueAt(a) + 7 * func.getValueAt(b);
+        double value = a + h;
+        for (int i = 1; i <= n - 1; i++) {
+            if (i % 4 == 0)
+                sum += 14 * func.getValueAt(value);
+            else if (i % 2 == 0)
+                sum += 12 * func.getValueAt(value);
+            else
+                sum += 32 * func.getValueAt(value);
+            value += h;
+        }
+
+        return sum * 2 * (h / 45);
+    }
 }
