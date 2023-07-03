4e22c4b9a489e0269948884d6543be92f044fdb9
Abd-HM <abd.alrrahman.alhamod@gmail.com>
2023-07-03 20:08:10 +0300

    Fix floating-point precision errors in getFormattedCoefficient method

--- src/Polynomial.java
+++ src/Polynomial.java
@@ -90,7 +90,7 @@ public class Polynomial {
      * @return the formatted coefficient as a string.
      */
     private String getFormattedCoefficient(double coeff) {
-        return (coeff == (int) coeff) ? String.valueOf((int) coeff) : String.valueOf(coeff);
+        return (coeff == (int) coeff) ? String.valueOf((int) coeff) : String.valueOf(Math.round(coeff * 1e10) / 1e10);
     }
 
     /**
