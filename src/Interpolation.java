9f1bc4846af464f610447d247df11d9484b64588
Abd-HM <abd.alrrahman.alhamod@gmail.com>
2023-07-03 18:40:49 +0300

    Add and Fix getNewtonGregoryForward with exceptions

--- src/Interpolation.java
+++ src/Interpolation.java
@@ -123,7 +123,7 @@ public class Interpolation {
             temp = yi1 - yi;
             temp = Math.round(temp * 1e10) / 1e10; //Rounding value back to fix floating-point precision errors
             q.add(temp);
-            if (i == n-2) {
+            if (i == n - 2) {
                 res.add(temp);
             }
             i++;
@@ -132,6 +132,45 @@ public class Interpolation {
                 n--;
                 q.poll();
             }
+        }
+        return res;
+    }
+
+    public static Polynomial getNewtonGregoryForward(Function func, int degree) {
+        if (func == null || degree < 0)
+            throw new ArithmeticException("invalid inputs");
+        ArrayList<Double> xp = func.getXp();
+        double h = xp.get(1) - xp.get(0);
+        for (int i = 1; i < xp.size() - 1; i++) {
+            if ((xp.get(i + 1) - xp.get(i)) != h) {
+                throw new ArithmeticException("step h is not static");
+            }
+        }
+        ArrayList<Double> Pcoeffs = new ArrayList<>(); //Creating coefficients Arraylist for P polynomial
+        Pcoeffs.add(-1 * xp.get(0)); // add -x0
+        Pcoeffs.add(1.0);            // add x
+        Polynomial P = new Polynomial(Pcoeffs); // Creating P Polynomial
+        P = P.multiply(1 / h);            // Dividing P on h
+        ArrayList<Double> coeffs = new ArrayList<>();
+        coeffs.add(0.0);
+        Polynomial res = new Polynomial(coeffs); //Creating coefficients Arraylist for res polynomial
+        ArrayList<Double> df0 = getNewtonGregoryForwardTable(func); //Getting Newton Gregory Forward Table values
+        res = res.add(df0.get(0)); // Adding y0
+        for (int i = 1; i <= degree; i++) {
+            coeffs = new ArrayList<>(); // //Creating coefficients Arraylist for Temp  polynomial
+            coeffs.add(1.0); //Initializing coefficients for Temp poly multiplication
+            Polynomial NGFPoly = new Polynomial(coeffs); //Create Temp poly
+            NGFPoly = NGFPoly.multiply(P); //Multiply by P
+            for (int j = 1; j < i; j++) {
+                NGFPoly = NGFPoly.multiply(P.add(-1 * j)); // Multiply by (P-1)(P-2)..
+            }
+            NGFPoly = NGFPoly.multiply(df0.get(i));// Multiply by df0
+            double factorial = 1;
+            for (int j = 2; j <= i; j++) {
+                factorial *= j;
+            }
+            NGFPoly = NGFPoly.multiply(1 / factorial); // Dividing by n!
+            res = res.add(NGFPoly);
         }
         return res;
     }
