c8b2316b7a016bcd88e93301ac89aa60ccd09a93
Abd-HM <abd.alrrahman.alhamod@gmail.com>
2023-07-05 15:35:42 +0300

    Add getNewtonDividesBackwardNoShorthand

--- src/Interpolation.java
+++ src/Interpolation.java
@@ -665,17 +665,48 @@ public class Interpolation {
         // the answer of interpolation by Newton Divides Forward
         StringBuilder sb = new StringBuilder();
         // append y0 to answer
-        sb.append(f0.get(0));
+        sb.append(getFormattedDouble(f0.get(0)));
         for (int i = 1; i <= degree; i++) {
             // append ' + ' to make .. + ..
             sb.append(" + ");
             // append f0i to answer
-            sb.append(f0.get(0));
+            sb.append(getFormattedDouble(f0.get(i)));
             sb.append(" ");
             for (int j = 0; j < i; j++) {
                 //init the current poly with the values
                 // a0 = -xj , a1 = 1 ; x - xj
                 Polynomial curr = new Polynomial(-1 * xp.get(j), 1);
+                // append the curr poly to answer with practices
+                sb.append('(');
+                sb.append(curr);
+                sb.append(')');
+            }
+        }
+        return sb.toString();
+    }
+
+    public static String getNewtonDividesBackwardNoShorthand(Function func, int degree) {
+        if (func == null || degree < 0)
+            throw new ArithmeticException("invalid inputs");
+        // get x points
+        ArrayList<Double> xp = func.getXp();
+        // get Newton Divides table values (lower diameter values)
+        ArrayList<Double> fn = getNewtonDividesBackwardTable(func);
+        // init string builder to create string representing
+        // the answer of interpolation by Newton Divides Forward
+        StringBuilder sb = new StringBuilder();
+        // append yn to answer
+        sb.append(getFormattedDouble(fn.get(0)));
+        for (int i = 1; i <= degree; i++) {
+            // append ' + ' to make .. + ..
+            sb.append(" + ");
+            // append fni to answer
+            sb.append(getFormattedDouble(fn.get(i)));
+            sb.append(" ");
+            for (int j = 0; j < i; j++) {
+                //init the current poly with the values
+                // a0 = -xn-j , a1 = 1 ; x - xn-j
+                Polynomial curr = new Polynomial(-1 * xp.get(degree - j), 1);
                 // append the curr poly to answer with practices
                 sb.append('(');
                 sb.append(curr);
