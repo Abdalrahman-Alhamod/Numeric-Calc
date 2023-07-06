fb6b18351d1ce12fd68f5cc0bf62cbf2a3ee6c6c
Abd-HM <abd.alrrahman.alhamod@gmail.com>
2023-07-07 00:46:12 +0300

    Add new constructor(string) to Function Update getValueAt with EvaluateExpression Add toString to Function

--- src/Function.java
+++ src/Function.java
@@ -6,8 +6,10 @@ public class Function {
     private ArrayList<Double> xp;
     private ArrayList<Double> yp;
 
-    public Function(String f) {
+    private String func;
 
+    public Function(String f) {
+        func = Objects.requireNonNull(f);
     }
 
     public Function(ArrayList<Double> xp, ArrayList<Double> yp) {
@@ -21,7 +23,17 @@ public class Function {
     public double getValueAt(double x) {
         //System.out.println("Value : " + x);
         //System.out.println("Func : " + yp.get(xp.indexOf(x)));
-        return yp.get(xp.indexOf(x));
+        if (yp != null)
+            return yp.get(xp.indexOf(x));
+        else {
+            double res;
+           // try {
+                res = EvaluateString.evaluate(func, x);
+            /*} catch (Exception e) {
+                throw new ArithmeticException("invalid function");
+            }*/
+            return res;
+        }
     }
 
     public double getDiffAt(double x, int rank) {
@@ -34,5 +46,14 @@ public class Function {
 
     ArrayList<Double> getYp() {
         return yp;
+    }
+
+    @Override
+    public String toString() {
+        if (func != null) {
+            return "F(x) = " + func;
+        } else {
+            return "X : " + xp + "\nY : " + yp;
+        }
     }
 }
