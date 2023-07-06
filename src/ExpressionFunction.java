cfbc6ca114b8410baa414fa50b9413126b9ce3e1
Abd-HM <abd.alrrahman.alhamod@gmail.com>
2023-07-07 00:59:36 +0300

    Add ExpressionFunction with its methods

---
+++ src/ExpressionFunction.java
@@ -0,0 +1,32 @@
+import java.util.Objects;
+
+public class ExpressionFunction {
+    private final String func;
+
+    public ExpressionFunction(String f) {
+        func = Objects.requireNonNull(f);
+    }
+
+    public double getValueAt(double x) {
+        double res;
+        try {
+            res = EvaluateString.evaluate(func, x);
+        } catch (Exception e) {
+            throw new ArithmeticException("invalid function");
+        }
+        return res;
+    }
+
+    public double getDiffAt(double x, int rank) {
+        return 0;
+    }
+
+    public double getIntegralAt(double x, int rank) {
+        return 0;
+    }
+
+    @Override
+    public String toString() {
+       return "F(x) = " + func;
+    }
+}
