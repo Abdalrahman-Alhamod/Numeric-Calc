bf0ee87a59e7ddf054ba4d4c70985baa4fe02e19
Abd-HM <abd.alrrahman.alhamod@gmail.com>
2023-07-07 00:59:49 +0300

    Add PointsFunction with its methods

---
+++ src/PointsFunction.java
@@ -0,0 +1,45 @@
+import java.util.ArrayList;
+import java.util.Objects;
+
+public class PointsFunction {
+    private final ArrayList<Double> xp;
+    private final ArrayList<Double> yp;
+
+    public PointsFunction(ArrayList<Double> xp, ArrayList<Double> yp) {
+        this.xp = Objects.requireNonNull(xp, "xp cannot be null");
+        this.yp = Objects.requireNonNull(yp, "yp cannot be null");
+        if (xp.size() != yp.size()) {
+            throw new ArithmeticException("number of function points mismatch");
+        }
+    }
+
+    public double getValueAt(double x) {
+        return yp.get(xp.indexOf(x));
+    }
+
+    public void addPoint(double x, double y) {
+        xp.add(x);
+        yp.add(y);
+    }
+
+    public double getDiffAt(double x, int rank) {
+        return 0;
+    }
+
+    public double getIntegralAt(double x, int rank) {
+        return 0;
+    }
+
+    ArrayList<Double> getXp() {
+        return xp;
+    }
+
+    ArrayList<Double> getYp() {
+        return yp;
+    }
+
+    @Override
+    public String toString() {
+        return "X : " + xp + "\nY : " + yp;
+    }
+}
