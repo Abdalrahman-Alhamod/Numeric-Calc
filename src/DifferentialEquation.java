0a3bbb56dcb70586b866d99c6e4596d3f7497501
Abd-HM <abd.alrrahman.alhamod@gmail.com>
2023-07-09 00:23:41 +0300

    Add Taylor method

--- src/DifferentialEquation.java
+++ src/DifferentialEquation.java
@@ -15,10 +15,11 @@ public class DifferentialEquation {
     public static class Euler {
         public static double solve(DifferentialEquation eq, double x0, double y0, double h, double x) {
             // init yi = y0 , xi = x0,  yi+1 = 0
-            double yi = y0, xi = x0,yi1 = 0;
+            double yi = y0, xi = x0, yi1 = 0;
             while (xi != x) {//if current value xi != x => continue
                 // get yi+1 = yi + yi' * h
                 yi1 = yi + eq.getValueAt(xi, yi) * h;
+                //System.out.println(yi1);
                 // update xi
                 xi = xi + h;
                 // update yi
@@ -28,6 +29,31 @@ public class DifferentialEquation {
         }
     }
 
+    public static class Taylor {
+        public static double solve(ArrayList<DifferentialEquation> eqs, double x0, double y0, double h, double x) {
+            // init yi = y0 , xi = x0,  yi+1 = 0
+            double yi = y0, xi = x0, yi1 = 0;
+            while (xi != x) {//if current value xi != x => continue
+                // init factorial = 1, sum = yi
+                double factor = 1, sum = yi;
+                for (int i = 0; i < eqs.size(); i++) {// for every Differential Equation
+                    // update factorial = 1 * 2 * 3 ... * n = n!
+                    factor *= (i + 1);
+                    // update ans = yi + ( yi'* h ) / 1! + ( yi'' * h^2 ) / 2! + ( yi''' * h^3 ) / 3! ...
+                    sum += eqs.get(i).getValueAt(xi, yi) * Math.pow(h, i + 1) * (1 / factor);
+                }
+                // update yi+1
+                yi1 = sum;
+                //System.out.println(yi1);
+                //update xi
+                xi = xi + h;
+                // update yi
+                yi = yi1;
+            }
+            return yi1;
+        }
+
+    }
 
 
 }
