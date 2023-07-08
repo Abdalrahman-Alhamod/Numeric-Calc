ec9782959c9112f91bf095f6120eb3fe566c5509
Abd-HM <abd.alrrahman.alhamod@gmail.com>
2023-07-08 23:43:05 +0300

    Create DifferentialEquation with constructor

---
+++ src/DifferentialEquation.java
@@ -0,0 +1,11 @@
+import java.util.Objects;
+
+public class DifferentialEquation {
+    String dy;
+
+    public DifferentialEquation(String dy) {
+        this.dy = Objects.requireNonNull(dy);
+    }
+
+
+}
