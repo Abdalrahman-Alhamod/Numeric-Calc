d6fb6987aca8bed5c945f8cffbd9dc409745e425
Abd-HM <abd.alrrahman.alhamod@gmail.com>
2023-07-08 23:44:37 +0300

    Add Euler method to DifferentialEquation

--- src/DifferentialEquation.java
+++ src/DifferentialEquation.java
@@ -10,7 +10,18 @@ public class DifferentialEquation {
         return EvaluateString.evaluate(dy, x, y);
     }
 
-
+    public static class Euler {
+        public static double solve(DifferentialEquation eq, double x0, double y0, double h, double x) {
+            double yi = y0, xi = x0;
+            double yi1 = 0;
+            while (xi != x) {
+                yi1 = yi + eq.getValueAt(xi, yi) * h;
+                xi = xi + h;
+                yi = yi1;
+            }
+            return yi1;
+        }
+    }
 
 
 }
