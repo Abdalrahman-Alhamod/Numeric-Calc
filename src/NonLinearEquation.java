76ab68ff2a6dece070498a0daf8ac42fafd95e14
Abd-HM <abd.alrrahman.alhamod@gmail.com>
2023-07-09 23:04:47 +0300

    Add FalsePosition class to NonLinearEquation

--- src/NonLinearEquation.java
+++ src/NonLinearEquation.java
@@ -8,21 +8,28 @@ public class NonLinearEquation {
     }
 
     public double getValueAt(double x) {
-        return EvaluateString.evaluate(eqs, x);
+        double res = EvaluateString.evaluate(eqs, x);
+        //Rounding value back to fix floating-point precision errors
+        res = Math.round(res * 1e10) / 1e10;
+        return res;
     }
 
     public double getValueAt(double x, double y) {
-        return EvaluateString.evaluate(eqs, x, y);
+        double res = EvaluateString.evaluate(eqs, x, y);
+        //Rounding value back to fix floating-point precision errors
+        res = Math.round(res * 1e10) / 1e10;
+        return res;
     }
 
     public static class Bisection {
         public static double solve(NonLinearEquation eq, double a, double b, double e) {
             double fa = eq.getValueAt(a), fb = eq.getValueAt(b);
-            //System.out.println("fa : " + fa + " fb : " + fb);
             double c = a;
             while (Math.abs(a - b) >= e) {
                 //System.out.println("a : " + a + " b : " + b + " c : " + c);
                 c = (a + b) / 2;
+                //Rounding value back to fix floating-point precision errors
+                c = Math.round(c * 1e10) / 1e10;
                 double fc = eq.getValueAt(c);
                 if (fc * fa < 0) {
                     b = c;
@@ -35,6 +42,40 @@ public class NonLinearEquation {
                     break;
             }
             return c;
+        }
+
+        public static double solve(NonLinearEquation eq, double a, double b) {
+            return solve(eq, a, b, 0.000000001);
+        }
+    }
+
+    public static class FalsePosition {
+        public static double solve(NonLinearEquation eq, double a, double b, double e) {
+            double fa = eq.getValueAt(a), fb = eq.getValueAt(b);
+            //System.out.println("fa : " + fa + " fb : " + fb );
+            double c = a;
+            while (Math.abs(a - b) >= e) {
+                c = ((a * fb) - (b * fa)) / (fb - fa);
+                //Rounding value back to fix floating-point precision errors
+                c = Math.round(c * 1e10) / 1e10;
+                //System.out.println("a : " + a + " b : " + b + " c : " + c);
+                double fc = eq.getValueAt(c);
+                //System.out.println("fa : " + fa + " fb : " + fb + " fc : " + fc);
+                if (fc * fa < 0) {
+                    b = c;
+                    fb = fc;
+                } else if (fc * fb < 0) {
+                    a = c;
+                    fa = fc;
+                }
+                if (Math.abs(a - b) < e || Math.abs(a - b) == 0 || fc == 0)
+                    break;
+            }
+            return c;
+        }
+
+        public static double solve(NonLinearEquation eq, double a, double b) {
+            return solve(eq, a, b, 0.000000001);
         }
     }
 
