e7eafbededc18d81f3d52e1c449f2454df2bec47
Abd-HM <abd.alrrahman.alhamod@gmail.com>
2023-07-09 21:52:03 +0300

    Add Bisection class to NonLinearEquation

--- src/NonLinearEquation.java
+++ src/NonLinearEquation.java
@@ -1,18 +1,41 @@
 import java.util.Objects;
 
 public class NonLinearEquation {
-    private final String eq;
+    private final String eqs;
 
     public NonLinearEquation(String eq) {
-        this.eq = Objects.requireNonNull(eq);
+        this.eqs = Objects.requireNonNull(eq);
     }
 
     public double getValueAt(double x) {
-        return EvaluateString.evaluate(eq, x);
+        return EvaluateString.evaluate(eqs, x);
     }
 
     public double getValueAt(double x, double y) {
-        return EvaluateString.evaluate(eq, x, y);
+        return EvaluateString.evaluate(eqs, x, y);
+    }
+
+    public static class Bisection {
+        public static double solve(NonLinearEquation eq, double a, double b, double e) {
+            double fa = eq.getValueAt(a), fb = eq.getValueAt(b);
+            //System.out.println("fa : " + fa + " fb : " + fb);
+            double c = a;
+            while (Math.abs(a - b) >= e) {
+                //System.out.println("a : " + a + " b : " + b + " c : " + c);
+                c = (a + b) / 2;
+                double fc = eq.getValueAt(c);
+                if (fc * fa < 0) {
+                    b = c;
+                    fb = fc;
+                } else if (fc * fb < 0) {
+                    a = c;
+                    fa = fc;
+                }
+                if (Math.abs(a - b) == 0)
+                    break;
+            }
+            return c;
+        }
     }
 
 }
