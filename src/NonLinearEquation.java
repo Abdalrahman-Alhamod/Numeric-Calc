69bec571a5f0cc396aa669fea9dfca08990b2e85
Abd-HM <abd.alrrahman.alhamod@gmail.com>
2023-07-10 00:43:59 +0300

    Restructure NonLinearEquation class

--- src/NonLinearEquation.java
+++ src/NonLinearEquation.java
@@ -1,36 +1,17 @@
 import java.util.Objects;
 
-public class NonLinearEquation {
-    private final String eqs;
-
-    public NonLinearEquation(String eq) {
-        this.eqs = Objects.requireNonNull(eq);
-    }
-
-    public double getValueAt(double x) {
-        double res = EvaluateString.evaluate(eqs, x);
-        //Rounding value back to fix floating-point precision errors
-        res = Math.round(res * 1e10) / 1e10;
-        return res;
-    }
-
-    public double getValueAt(double x, double y) {
-        double res = EvaluateString.evaluate(eqs, x, y);
-        //Rounding value back to fix floating-point precision errors
-        res = Math.round(res * 1e10) / 1e10;
-        return res;
-    }
+public abstract class NonLinearEquation {
 
     public static class Bisection {
-        public static double solve(NonLinearEquation eq, double a, double b, double e) {
-            double fa = eq.getValueAt(a), fb = eq.getValueAt(b);
+        public static double solve(ExpressionFunction fx, double a, double b, double e) {
+            double fa = fx.getValueAt(a), fb = fx.getValueAt(b);
             double c = a;
             while (Math.abs(a - b) >= e) {
                 //System.out.println("a : " + a + " b : " + b + " c : " + c);
                 c = (a + b) / 2;
                 //Rounding value back to fix floating-point precision errors
                 c = Math.round(c * 1e10) / 1e10;
-                double fc = eq.getValueAt(c);
+                double fc = fx.getValueAt(c);
                 if (fc * fa < 0) {
                     b = c;
                     fb = fc;
@@ -44,21 +25,21 @@ public class NonLinearEquation {
             return c;
         }
 
-        public static double solve(NonLinearEquation eq, double a, double b) {
-            return solve(eq, a, b, 0.000000001);
+        public static double solve(ExpressionFunction fx, double a, double b) {
+            return solve(fx, a, b, 0.000000001);
         }
     }
 
     public static class FalsePosition {
-        public static double solve(NonLinearEquation eq, double a, double b, double e) {
-            double fa = eq.getValueAt(a), fb = eq.getValueAt(b);
+        public static double solve(ExpressionFunction fx, double a, double b, double e) {
+            double fa = fx.getValueAt(a), fb = fx.getValueAt(b);
             double c = a;
             while (Math.abs(a - b) >= e) {
                 c = ((a * fb) - (b * fa)) / (fb - fa);
                 //Rounding value back to fix floating-point precision errors
                 c = Math.round(c * 1e10) / 1e10;
                 //System.out.println("a : " + a + " b : " + b + " c : " + c);
-                double fc = eq.getValueAt(c);
+                double fc = fx.getValueAt(c);
                 //System.out.println("fa : " + fa + " fb : " + fb + " fc : " + fc);
                 if (fc * fa < 0) {
                     b = c;
@@ -73,22 +54,22 @@ public class NonLinearEquation {
             return c;
         }
 
-        public static double solve(NonLinearEquation eq, double a, double b) {
-            return solve(eq, a, b, 0.000000001);
+        public static double solve(ExpressionFunction fx, double a, double b) {
+            return solve(fx, a, b, 0.000000001);
         }
     }
 
     public static class Secant {
-        public static double solve(NonLinearEquation eq, double x0, double x1, double e) {
+        public static double solve(ExpressionFunction fx, double x0, double x1, double e) {
             double xi_1 = x0, xi = x1;
-            double fxi_1 = eq.getValueAt(x0), fxi = eq.getValueAt(x1);
+            double fxi_1 = fx.getValueAt(x0), fxi = fx.getValueAt(x1);
             double xi1 = xi_1;
             while (true) {
                 xi1 = xi - ((xi - xi_1) / (fxi - fxi_1)) * fxi;
                 //Rounding value back to fix floating-point precision errors
                 xi1 = Math.round(xi1 * 1e10) / 1e10;
                 //System.out.println("xi-1 : " + xi_1 + " xi : " + xi + " xi+1 : " + xi1);
-                double fxi1 = eq.getValueAt(xi1);
+                double fxi1 = fx.getValueAt(xi1);
                 //System.out.println("f(xi-1) : " + fxi_1 + " f(xi) : " + fxi + " f(xi+1) : " + fxi1);
                 if (Math.abs(xi1 - xi) < e || Math.abs(xi1 - xi) == 0 || fxi1 == 0)
                     break;
@@ -100,8 +81,8 @@ public class NonLinearEquation {
             return xi1;
         }
 
-        public static double solve(NonLinearEquation eq, double a, double b) {
-            return solve(eq, a, b, 1E-9);
+        public static double solve(ExpressionFunction fx, double a, double b) {
+            return solve(fx, a, b, 1E-9);
         }
     }
 
