48c1281b4fd6a5848488ffc798db7d40c1a1a58a
Abd-HM <abd.alrrahman.alhamod@gmail.com>
2023-07-26 04:51:25 +0300

    Add JAVA DOC to SettingsManager

--- src/Util/SettingsManager.java
+++ src/Util/SettingsManager.java
@@ -3,10 +3,21 @@ package Util;
 import java.io.*;
 import java.util.Properties;
 
+/**
+ * The SettingsManager class handles reading and writing application settings to a properties file.
+ * It provides methods to get and set various settings such as light theme, dark theme, and accuracy.
+ * Settings are saved to a file named "config.properties" in the current directory.
+ */
+@SuppressWarnings("all")
 public class SettingsManager {
     private static final String PROPERTIES_FILE = "config.properties";
     private final Properties properties;
 
+    /**
+     * Constructs a new SettingsManager instance.
+     * Initializes the properties object and loads settings from the "config.properties" file if it exists.
+     * If the file does not exist, default settings are used, and a new "config.properties" file is created.
+     */
     public SettingsManager() {
         properties = new Properties();
         try {
@@ -27,6 +38,10 @@ public class SettingsManager {
         }
     }
 
+    /**
+     * Saves the current settings to the "config.properties" file.
+     * If an I/O exception occurs during the save process, it is caught and ignored.
+     */
     public void saveSettings() {
         try {
             OutputStream outputStream = new FileOutputStream(PROPERTIES_FILE);
@@ -37,26 +52,56 @@ public class SettingsManager {
         }
     }
 
+    /**
+     * Gets the currently set light theme.
+     *
+     * @return The name of the current light theme, or "Light Flat" as the default if not set.
+     */
     public String getLightTheme() {
         return properties.getProperty("lightTheme", "Light Flat");
     }
 
+    /**
+     * Sets the light theme.
+     *
+     * @param theme The name of the light theme to set.
+     */
     public void setLightTheme(String theme) {
         properties.setProperty("lightTheme", theme);
     }
 
+    /**
+     * Gets the currently set dark theme.
+     *
+     * @return The name of the current dark theme, or "One Dark" as the default if not set.
+     */
     public String getDarkTheme() {
         return properties.getProperty("darkTheme", "One Dark");
     }
 
+    /**
+     * Sets the dark theme.
+     *
+     * @param theme The name of the dark theme to set.
+     */
     public void setDarkTheme(String theme) {
         properties.setProperty("darkTheme", theme);
     }
 
+    /**
+     * Gets the currently set accuracy value.
+     *
+     * @return The current accuracy value, or 10 as the default if not set.
+     */
     public int getAccuracy() {
         return Integer.parseInt(properties.getProperty("accuracy", "10"));
     }
 
+    /**
+     * Sets the accuracy value.
+     *
+     * @param accuracy The accuracy value to set.
+     */
     public void setAccuracy(int accuracy) {
         properties.setProperty("accuracy", Integer.toString(accuracy));
     }
