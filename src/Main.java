0967de1a4f2e2038c03f8d3e7ecc59e3a2dd5642
Abd-HM <abd.alrrahman.alhamod@gmail.com>
2023-08-30 02:47:56 +0300

    Build the project with the new jar and apply the new changes and settings

--- src/Main.java
+++ src/Main.java
@@ -20,7 +20,7 @@ import javax.swing.*;
  * <a href="mailto:abd.alrrahman.alhamod@gmail.com">E-mail</a><br>
  * <a href="https://github.com/Abdalrahman-Alhamod">Github</a><br>
  * <a href="https://www.linkedin.com/in/abd-alrrahman-alhamod/">LinkedIn</a><br>
- * <a href="https://www.facebook.com/profile.php?id=100011427430343">Facebook</a>
+ * <a href="https://t.me/Abd_Alrhman_Alhamod">Telegram</a>
  */
 public class Main {
     public static void main(String[] args) {
